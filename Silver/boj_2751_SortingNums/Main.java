4ccb7d974ea0d62a276b89c21394531b32535cdd
JinwonYun <144454379+JinwonYun@users.noreply.github.com>
2025-08-05 12:07:40 +0900

    boj_2751_SortingNums

---
+++ Silver/boj_2751_SortingNums/Main.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] nums = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(nums);
+		
+		for(int i = 0; i < N; i++) {
+			sb.append(nums[i]).append("\n");
+		}
+		
+		System.out.println(sb);
+		
+	}
+	
+}
