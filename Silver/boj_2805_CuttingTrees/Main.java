d64bbae3e139504490d2c12d26cdbe399d5c4254
JinwonYun <144454379+JinwonYun@users.noreply.github.com>
2025-08-19 00:23:15 +0900

    boj_2805_CuttingTrees

---
+++ Silver/boj_2805_CuttingTrees/Main.java
@@ -0,0 +1,56 @@
+aimport java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(st.nextToken());
+		long k = Long.parseLong(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		long[] trees = new long[n];
+		long max = 0;
+		
+		for(int i = 0; i < n; i++) {
+			trees[i] = Integer.parseInt(st.nextToken());
+			max = Math.max(max, trees[i]);
+		}
+		
+		long result = upperBound(trees, k, max) - 1;
+		sb.append(result);
+		
+		System.out.println(sb);
+		
+	}
+	
+	public static long upperBound(long[] input, long k, long maxHeight) {
+		long start = 0;
+		long end = maxHeight + 1;
+		
+		while(start < end) {
+			long mid = (start + end) / 2;
+			long cnt = 0;
+			
+			for(long tree : input) {
+				if(tree > mid) {
+					cnt += tree - mid;
+				}
+			}
+			
+			if(cnt < k) {
+				end = mid;
+			} else {
+				start = mid + 1;
+			}
+		}
+		
+		return end;
+	}
+
+}
