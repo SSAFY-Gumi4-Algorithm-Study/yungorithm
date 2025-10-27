f73f248b048e88a5241f8846b648098692989087
JinwonYun <144454379+JinwonYun@users.noreply.github.com>
2025-10-27 15:42:01 +0900

    boj_14500_Tetrominoes

---
+++ Gold/boj_14500_Tetrominoes/Main.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int n, m;
+	static int[][] map;
+	static boolean[][] visited;
+	static int[] dx = { -1, 1, 0, 0 };
+	static int[] dy = { 0, 0, -1, 1 };
+	static int result;
+
+    public static void main(String[] args) throws IOException {
+    	
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        map = new int[n][m];
+        for(int i = 0; i < n; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	for(int j = 0; j < m; j++) {
+        		map[i][j] = Integer.parseInt(st.nextToken());
+        	}
+        }
+        
+        visited = new boolean[n][m];
+        for(int i = 0; i < n; i++) {
+        	for(int j = 0; j < m; j++) {
+        		visited[i][j] = true;
+        		dfs(i, j, 1, map[i][j]);
+        		visited[i][j] = false;
+        	}
+        }
+        
+        System.out.println(result);
+        
+    }
+    
+    public static void dfs(int x, int y, int depth, int sum) {
+    	if(depth == 4) {
+    		result = Math.max(result, sum);
+    		return;
+    	}
+    	
+    	for(int i = 0; i < 4; i++) {
+    		int nx = x + dx[i];
+    		int ny = y + dy[i];
+    		
+    		if(nx < 0 || ny < 0 || nx >= n || ny >= m) {
+    			continue;
+    		}
+    		
+    		if(visited[nx][ny]) {
+    			continue;
+    		}
+    		
+    		visited[nx][ny] = true;
+    		
+    		if(depth == 2) {
+    			dfs(x, y, depth + 1, sum + map[nx][ny]);
+    		}
+    		
+    		dfs(nx, ny, depth + 1, sum + map[nx][ny]);
+    		
+    		visited[nx][ny] = false;
+    	}
+    }
+    
+}
