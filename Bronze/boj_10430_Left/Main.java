2cf4be7ec58ef98eed5109cdad3f87c25e071de6
JinwonYun <144454379+JinwonYun@users.noreply.github.com>
2025-11-07 20:36:40 +0900

    boj_10430_Left

---
+++ Bronze/boj_10430_Left/Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int a, b, c;
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		sb.append((a + b) % c).append("\n");
+		sb.append(((a % c) + (b % c)) % c).append("\n");
+		sb.append((a * b) % c).append("\n");
+		sb.append(((a % c) * (b % c)) % c).append("\n");
+		
+		System.out.println(sb);
+		
+	}
+
+}
