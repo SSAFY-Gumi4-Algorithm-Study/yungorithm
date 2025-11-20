f9db6557a56d949229f0be2fa4146c3599c447c4
JinwonYun <144454379+JinwonYun@users.noreply.github.com>
2025-11-20 14:18:24 +0900

    boj_7682_TicTacToe

--- Gold/boj_7682_TicTacToe/Main.java
+++ Gold/boj_7682_TicTacToe/Main.java
@@ -52,7 +52,7 @@ public class Main {
 
 	public static boolean isValid() {
 	    int x = 0;
-      int o = 0;
+      	int o = 0;
 	    for (int i = 0; i < 3; i++) {
 	        for (int j = 0; j < 3; j++) {
 	            if (arr[i][j] == 'X') x++;
