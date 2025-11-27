c795beeb25c5b06b512fb472d9891b9d06468304
JinwonYun <144454379+JinwonYun@users.noreply.github.com>
2025-11-28 00:33:28 +0900

    boj_15663_NandM9

---
+++ Silver/boj_15663_NandM9/Main.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int n, m, resultIdx;
+	static int[] arr, result;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
+    	
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine());
+        arr = new int[n];
+        for(int i = 0; i < n; i++) {
+        	arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr);
+        
+    	result = new int[m];
+    	visited = new boolean[n];
+    	dfs(0);
+    	
+    	System.out.println(sb);
+    	
+    }
+    
+    public static void dfs(int depth) {
+    	if(depth == m) {
+    		for(int i = 0; i < m; i++) {
+    			sb.append(result[i]).append(' ');   			
+    		}
+    		sb.append("\n");
+    		return;
+    	}
+    	
+    	int prev = 10001;
+    	for(int i = 0; i < n; i++) {
+    		if(visited[i] || arr[i] == prev) continue;
+    		
+    		prev = arr[i];
+    		visited[i] = true;
+    		result[depth] = arr[i];
+    		dfs(depth + 1);
+    		visited[i] = false;
+    	}
+    }
+    
+}
