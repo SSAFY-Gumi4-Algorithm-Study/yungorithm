f5474947b005ff73359d3717d277467c9a7c7c55
JinwonYun <dnj1510@naver.com>
2026-04-19 20:33:53 +0900

    [BOJ] 1937. 욕심쟁이 판다

---
+++ boj/Gold/1937.욕심쟁이 판다/Main.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static int[][] map, dp;
+    static int[] dx = { -1, 1, 0, 0 };
+    static int[] dy = { 0, 0, -1, 1 };
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+
+        map = new int[n][n];
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int result = 0;
+        dp = new int[n][n];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                result = Math.max(result, dfs(i, j));
+            }
+        }
+
+        System.out.println(result);
+
+    }
+
+    private static int dfs(int x, int y) {
+        if(dp[x][y] != 0) return dp[x][y];
+
+        dp[x][y] = 1;
+
+        for(int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+
+            if(map[nx][ny] > map[x][y]) {
+                dp[x][y] = Math.max(dp[x][y], dfs(nx, ny) + 1);
+            }
+        }
+
+        return dp[x][y];
+    }
+
+}
+
+
