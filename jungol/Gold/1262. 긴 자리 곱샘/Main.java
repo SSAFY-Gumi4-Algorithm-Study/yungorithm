e04937c8ed27895d6512ad7f756ea876f8290d2e
윤진원 <dnj1510@naver.com>
2026-06-28 23:47:06 +0900

    [JO] 1262. 긴 자리 곱샘

---
+++ jungol/Gold/1262. 긴 자리 곱샘/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        while(true) {
+            st = new StringTokenizer(br.readLine());
+
+            BigInteger n = new BigInteger(st.nextToken());
+            if(!st.hasMoreTokens()) {
+                break;
+            }
+            BigInteger m = new BigInteger(st.nextToken());
+
+            sb.append(n.multiply(m)).append('\n');
+        }
+
+        System.out.println(sb);
+
+    }
+
+}
